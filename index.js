1554f6a81960e94d07a533cda0f9a52aa6140d73
DilshanWNA <dilshanwarnasuriyaa@gmail.com>
2024-11-11 00:48:22 +0530

    connect to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose";
 
 const app = express();
+
+dotenv.config();
+
+// Database connection
+mongoose.connect(process.env.CONNECTION_STRING).then(() => console.log("Database connection success")).catch(() => console.log("Database connection fail"))
 
 app.listen(5000, (req, res) => {
     console.log("The program runs on port 5000");
