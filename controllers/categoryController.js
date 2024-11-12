5ee1f116b0dccea4daacf809b811c21b8e4d9d4a
DilshanWNA <dilshanwarnasuriyaa@gmail.com>
2024-11-12 16:56:13 +0530

    Gel all category api done and add sum validation to save category api

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,18 +3,35 @@ import { isAdmin } from "./userController.js";
 
 export function persist(req, res) {
     if (!isAdmin(req)) {
-        return res.status(401).json({ message: "Admin access required"});
+        return res.status(401).json({ message: "Admin access required" });
     }
 
     const newCategory = new Category(req.body);
     newCategory.save()
         .then((category) => {
-            res.json({
-                message: "Category creation Successful",
+            res.status(201).json({
+                message: "Category Creation Successful",
                 category: category
             })
         })
         .catch((err) => {
-            res.status(500).json({ message: "Server error occurred", error: err.message });
+            const errorMessage = err.errorResponse.errmsg;
+            if (errorMessage.includes("name_1")) {
+                res.status(409).json({ message: "Category is already created" })
+            }
+            else {
+                res.status(500).json({ message: "Server error occurred", error: err.message });
+            }
         })
+}
+
+export function retrieve(req, res) {
+    Category.find().then((categories) => {
+        if (categories.length === 0) {
+            return res.status(404).json({ message: "No categories found" });
+        }
+        res.status(200).json(categories);
+    }).catch((err) => {
+        res.status(500).json({ message: "Server error occurred", error: err.message });
+    })
 }
