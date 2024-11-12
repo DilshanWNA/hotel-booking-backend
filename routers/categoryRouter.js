9d3bdc6f2cd998ca5739ed2922cc02ceb8b5be07
DilshanWNA <dilshanwarnasuriyaa@gmail.com>
2024-11-12 17:02:16 +0530

    Category update api done

--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { persist, retrieve } from '../controllers/categoryController.js';
+import { persist, retrieve, update } from '../controllers/categoryController.js';
 
 const categoryRouter = express.Router();
 
 categoryRouter.post("/", persist);
 
 categoryRouter.get("/", retrieve);
+
+categoryRouter.put("/", update);
 
 export default categoryRouter;
