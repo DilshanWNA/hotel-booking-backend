a1fbe1e896b040d10a474cb60ff5ee06733f315d
DilshanWNA <dilshanwarnasuriyaa@gmail.com>
2024-11-14 21:08:49 +0530

    Event update api done

--- routers/eventRouter.js
+++ routers/eventRouter.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { persist, retrieve } from '../controllers/eventController.js';
+import { persist, retrieve, update } from '../controllers/eventController.js';
 
 const eventRouter = express.Router();
 
 eventRouter.post("/", persist);
 
 eventRouter.get("/", retrieve);
+
+eventRouter.put("/", update);
 
 export default eventRouter;
