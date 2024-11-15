7f8c28bd27e130429083eb48221a7550580aa106
DilshanWNA <dilshanwarnasuriyaa@gmail.com>
2024-11-15 13:35:14 +0530

    Get all review api done

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -27,3 +27,16 @@ export function persist(req, res) {
             }
         })
 }
+
+export function retrieve(req, res) {
+    Review.find()
+        .then((reviews) => {
+            if (reviews.length === 0) {
+                return res.status(404).json({ message: "Review not found" });
+            }
+            res.status(200).json(reviews);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: "Server error occurred", error: err.message });
+        })
+}
